b0a8009da46045e4246c6f01cc1cd0f528adbe83
Berka828 <Berka_ngong@yahoo.com>
2026-03-25 18:38:29 -0400

    Update game.js

--- game.js
+++ game.js
@@ -76,7 +76,7 @@ const CONTACT_DISTANCE = 68;
 const BAT_LENGTH = 132;
 
 const SKELETON_SCALE = 0.68;
-const SKELETON_OFFSET_Y = 999;
+const SKELETON_OFFSET_Y = 499;
 const SKELETON_OFFSET_X = -300;
 
 // ---------- AUDIO ----------
